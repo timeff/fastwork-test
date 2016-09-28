9531c42d6421b8dc70fa0e39c79a0f6f184214b4
timeff <kawin.ac125@gmail.com>
2016-09-28 20:42:08 +0700

    add some comments and prepare for deployment

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 new Vue({
     el: '#app',
     data: {
-        show:false,
+        show:false, // Boolean for triggle result on view page
         user:{},
         repos:[],
-        error:''
+        error:'' // Error text
     },
     methods:{
         getData:function(){
             var inputUser = this.inputUser.trim()
+
+            //API CALL - User Data
             axios.get('https://api.github.com/users/'+inputUser)
             .then(response=>{
                 this.user=response.data
-                this.show=true
                 this.inputUser=''
+
+                // Show the result and hide error text
+                this.show=true
                 this.error=''
             })
             .then(()=>{
+                //API-CALL Repos Data
                 axios.get('https://api.github.com/users/'+inputUser+'/repos')
                 .then(response=>{
                     this.repos=response.data
                 })
             })
             .catch(()=>{
+                //In case there is an error
                 this.error=" Can't find your name Dude!"    
             })
         }
